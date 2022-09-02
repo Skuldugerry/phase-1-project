5d88408921ddfe4695a0a22090b76782f60141b1
Hillary Chemiron <chemiron.hillary@student.moringaschool.com>
2022-09-02 09:28:21 +0300

    Completed assignment

--- data.js
+++ data.js
@@ -1,13 +1,15 @@
 
+// Fetch anime list from server
 function getAnimeFacts(){
     fetch("https://anime-facts-rest-api.herokuapp.com/api/v1")
     .then(function (resp){
         return resp.json()
     })
     .then((data)=>data.data.forEach(animeFact => {revealFacts(animeFact)}))
-}
+    }
 getAnimeFacts()
 
+// show anime list on webPage
 function revealFacts(animeFact){
     let card=document.createElement('li')
     card.className="existing-anime-facts"
@@ -21,3 +23,57 @@ function revealFacts(animeFact){
     document.getElementById("existing_facts").appendChild(card)
 }
 
+//  Event listner on add button for filling both Anime Name and Image URL
+document.addEventListener('DOMContentLoaded',()=>{
+document.querySelector('form').addEventListener('submit',(e)=>{
+    e.preventDefault()
+    let newAnime={
+        name:e.target.animeName.value,
+        ImageURL:e.target.img_url.value,
+    }
+    addAnime(newAnime)
+}
+)})
+
+// Clicking Add button on anime Name and ImageURL adds details to server(POST request)
+function addAnime(newAnime){
+    fetch("https://anime-facts-rest-api.herokuapp.com/api/v1",{
+        method:"POST",
+        headers:{
+            "Content-Type":"Application/json"
+        },
+        body:JSON.stringify(newAnime)
+    })
+    .then(res=>res.json())
+    .then((anime)=>console.log(anime))
+}
+
+// Add Anime Name button event listener
+document.querySelector('form').addEventListener('submit',((e)=>{
+    let animeName= e.target.animeName.value
+    addAnimeName(animeName)}))
+
+//  POST Anime Name to server
+function addAnimeName(animeName){
+    fetch("https://anime-facts-rest-api.herokuapp.com/api/v1",{
+        method:"POST",
+        headers: {
+            "Content-Type":"Application/json"
+        },
+        body:JSON.stringify(animeName)
+    })
+    .then((res)=>res.json())
+    .then((newAnimeName)=>showAnimeName(newAnimeName))
+}
+
+//  show Anime Name on page
+function showAnimeName(newAnimeName){
+    let sentry=document.createElement('ul')
+    sentry.innerHTML=`
+        <li>${newAnimeName}</li>
+        <img alt="${newAnimeName}"
+    `
+
+    console.log(sentry)
+}
+showAnimeName()
